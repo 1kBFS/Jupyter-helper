5822fdfccb28a86842a146192598b92380da7786
= <=>
2023-10-18 18:27:35 +0300

    fix tests

--- main.py
+++ main.py
@@ -28,14 +28,20 @@ def start(num_users, base_dir='./', session_name="jupyter"):
     """
     Запустить $num_users ноутбуков. У каждого рабочая директория $base_dir+$folder_num
     """
-    if num_users < 0:
-        raise ValueError("Number of notebooks is positive.")
+    if num_users <= 0:
+        # raise ValueError("Number of notebooks is positive.")
+        print("Number of notebooks is positive.")
+        return 
     base_dir = base_dir + "/" if base_dir[-1] != '/' else base_dir
     if not os.path.exists(base_dir):
-        raise ValueError("No such base directory. ")
+        # raise ValueError("No such base directory. ")
+        print("No such base directory. ")
+        return
     server = libtmux.Server()
     if server.has_session(target_session=session_name):
-        raise RuntimeWarning("The session with jupyter notebooks is already running, the notebooks are deployed.")
+        # raise RuntimeWarning("The session with jupyter notebooks is already running, the notebooks are deployed.")
+        print("The session with jupyter notebooks is already running, the notebooks are deployed.")
+        return
     server.new_session(session_name=session_name, start_directory=base_dir)
     session = server.sessions.get(session_name=session_name)
     for i in tqdm.trange(num_users):
@@ -46,6 +52,7 @@ def start(num_users, base_dir='./', session_name="jupyter"):
                            window_shell=f"jupyter notebook --port {PORT + i} --no-browser --NotebookApp.token={token} --NotebookApp.notebook_dir={base_dir}dir{i}",
                            start_directory=f"{base_dir}dir{i}")
         click.echo(f"jupyter notebook number {i}: port {PORT + i}, token {token}")
+    session.kill_window("0")
 
 
 @click.command()
@@ -56,8 +63,13 @@ def stop(session_name, num):
     @:param session_name: Названия tmux-сессии, в которой запущены окружения
     @:param num: номер ноутбука, кот. можно убить
     """
-    session = connect_to_session(session_name)
-    session.kill_window(f"notebook {num}")
+    try:
+        session = connect_to_session(session_name)
+        session.kill_window(f"notebook {num}")
+    except RuntimeWarning as ex:
+        print(ex)
+    except libtmux.exc.LibTmuxException as ex:
+        print(ex)
 
 
 @click.command(name="stop_all")
@@ -66,8 +78,11 @@ def stop_all(session_name):
     """
     @:param session_name: Названия tmux-сессии, в которой запущены окружения
     """
-    session = connect_to_session(session_name)
-    session.kill_session()
+    try:
+        session = connect_to_session(session_name)
+        session.kill_session()
+    except RuntimeWarning as ex:
+        print(ex)
 
 
 if __name__ == '__main__':
